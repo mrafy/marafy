2b3e892fdfb8897e0a28eabfa9723d758eb59067
mrafy <56656813+mrafy@users.noreply.github.com>
2019-11-30 22:25:17 +0300

    Create goads.js

---
+++ goads.js
@@ -0,0 +1,13 @@
+<div align="center">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- متجاوبة -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4865213097564417"
+     data-ad-slot="9605554750"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
